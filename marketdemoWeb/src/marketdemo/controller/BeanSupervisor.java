258eff64fb437f6716b4f981cf4f34841fabab9f
mreap <mrea@utn.edu.ec>
2019-07-25 01:20:25 -0500

    Adicion de reportes jasper

--- marketdemoWeb/src/marketdemo/controller/BeanSupervisor.java
+++ marketdemoWeb/src/marketdemo/controller/BeanSupervisor.java
@@ -1,16 +1,26 @@
 package marketdemo.controller;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 
 import marketdemo.model.entities.PedidoCab;
 import marketdemo.model.manager.ManagerPedidos;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 
 @Named
 @SessionScoped
@@ -66,6 +76,36 @@ public class BeanSupervisor  implements Serializable {
 			return null;
 		}
 	}
+	
+	public String actionReporte() {
+		Map<String, Object> parametros = new HashMap<String, Object>();
+		/*
+		 * parametros.put("p_titulo_principal",p_titulo_principal);
+		 * parametros.put("p_titulo",p_titulo);
+		 */
+		FacesContext context = FacesContext.getCurrentInstance();
+		ServletContext servletContext = (ServletContext) context.getExternalContext().getContext();
+		String ruta = servletContext.getRealPath("supervisor/clientes.jasper");
+		System.out.println(ruta);
+		HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+		response.addHeader("Content-disposition", "attachment;filename=blogs.pdf");
+		response.setContentType("application/pdf");
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection connection = null;
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/marketdemo", "postgres", "postgres");
+			JasperPrint impresion = JasperFillManager.fillReport(ruta, parametros, connection);
+			JasperExportManager.exportReportToPdfStream(impresion, response.getOutputStream());
+			context.getApplication().getStateManager().saveView(context);
+			System.out.println("reporte generado.");
+			context.responseComplete();
+		} catch (Exception e) {
+			JSFUtil.crearMensajeERROR(e.getMessage());
+			e.printStackTrace();
+		}
+		return "";
+	}
+	
 	public Date getFechaInicio() {
 		return fechaInicio;
 	}
